55d795f1b80bef5e0fbc69ee62e7a9446ae2668c
idan elbaz <51115403+idanelbaz@users.noreply.github.com>
2019-12-04 19:27:32 +0200

    Sprint 1 - Docker
    * added postman collection of sefaria api.
    build for windows.
    expose mongo port in docker-compose.yml.
    local_settings.py updated from project, otherwise failed to run.
    
    * find book and chapter by index
    
    * aws added

---
+++ aws_push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+eval $(aws ecr get-login --no-include-email)
+docker tag "sefaria-docker_web:latest" "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-api:latest"
+docker tag "sefaria-docker_mongo:latest" "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-db:latest"
+docker push "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-api:latest"
+docker push "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-db:latest"
