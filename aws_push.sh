cf0b6cf7c4f09598d29dcd074afa8663b49a9cd8
shacharmo <shachar.mossek@gmail.com>
2020-01-02 18:53:49 +0200

    Add cache (#3)
    * Added cache service
    
    * added volumes to cache
    
    Co-authored-by: Ben Edery <47628503+benedery@users.noreply.github.com>

--- aws_push.sh
+++ aws_push.sh
@@ -2,5 +2,7 @@
 eval $(aws ecr get-login --no-include-email)
 docker tag "sefaria-docker_web:latest" "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-api:latest"
 docker tag "sefaria-docker_mongo:latest" "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-db:latest"
+docker tag "sefaria-docker_cache:latest" "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-cache-api:latest"
 docker push "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-api:latest"
 docker push "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-db:latest"
+docker push "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-cache-api:latest"
