f127481eee4827c34a841433341342000d1322ba
shacharm <shachar_m@webiks.com>
2020-01-02 18:25:40 +0200

    Added cache service

--- aws_push.sh
+++ aws_push.sh
@@ -4,3 +4,4 @@ docker tag "sefaria-docker_web:latest" "058331968334.dkr.ecr.eu-central-1.amazon
 docker tag "sefaria-docker_mongo:latest" "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-db:latest"
 docker push "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-api:latest"
 docker push "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-sefaria-db:latest"
+docker push "058331968334.dkr.ecr.eu-central-1.amazonaws.com/shitufta-cache-db:latest"
